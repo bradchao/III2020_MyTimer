07ce547d0f5848fb68deea1b779613628f4f4cc7
bradchao <bk400288>
2020-07-25 09:56:56 +0800

    20072504

--- app/src/main/java/tw/brad/apps/mytimer/MainActivity.java
+++ app/src/main/java/tw/brad/apps/mytimer/MainActivity.java
@@ -4,18 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button leftBtn, rightBtn;
+    private boolean isStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        leftBtn = findViewById(R.id.leftBtn);
+        rightBtn = findViewById(R.id.rightBtn);
+
     }
 
     public void doLeft(View view) {
     }
 
     public void doRight(View view) {
+        isStart = !isStart;
+        if (isStart){
+            leftBtn.setText("LAP");
+            rightBtn.setText("STOP");
+        }else{
+            leftBtn.setText("RESET");
+            rightBtn.setText("START");
+        }
     }
 }
